d28fa4f1a77dfe6549d3b5ae28519534845fa73a
svininykh <svininykh@gmail.com>
2014-09-28 16:50:37 +0600

    Add Apache license to source code.

--- src/main/java/mobi/nordpos/catalog/action/ProductCategoryActionBean.java
+++ src/main/java/mobi/nordpos/catalog/action/ProductCategoryActionBean.java
@@ -1,5 +1,17 @@
 /**
  * Copyright (c) 2012-2014 Nord Trading Network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package mobi.nordpos.catalog.action;
 
@@ -26,7 +38,7 @@ public class ProductCategoryActionBean extends BaseActionBean {
 
     private String name;
     private String code;
-    
+
     @DefaultHandler
     public Resolution list() throws SQLException {
         return new ForwardResolution(CATEGORY_LIST);
@@ -73,7 +85,7 @@ public class ProductCategoryActionBean extends BaseActionBean {
     public List<ProductCategory> getCategoryList() throws SQLException {
         try {
             this.connection = new JdbcConnectionSource(getDataBaseURL(), getDataBaseUser(), getDataBasePassword());
-            ProductCategoryPersist productCategoryDao = new ProductCategoryPersist(connection);         
+            ProductCategoryPersist productCategoryDao = new ProductCategoryPersist(connection);
             return productCategoryDao.getList();
         } finally {
             if (connection != null) {
